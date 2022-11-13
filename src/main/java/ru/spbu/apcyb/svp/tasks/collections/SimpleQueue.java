2568160fcdf11ea1e89eac1379b3c862da742e64
Aleksei Chashin <jabuj@yandex-team.ru>
2022-11-13 17:46:11 +0300

    Task 2

---
+++ src/main/java/ru/spbu/apcyb/svp/tasks/collections/SimpleQueue.java
@@ -0,0 +1,116 @@
+package ru.spbu.apcyb.svp.tasks.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.apache.maven.surefire.shared.lang3.NotImplementedException;
+
+/**
+ * Standard queue.
+ */
+public class SimpleQueue<T> implements java.util.Queue<T> {
+
+    LinkedList<T> list;
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public boolean add(T t) {
+        return list.add(t);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    @Override
+    public T remove() {
+        if (list.size() == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return list.remove(0);
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public boolean offer(T t) {
+        return add(t);
+    }
+
+    @Override
+    public T poll() {
+        if (list.size() == 0) {
+            return null;
+        }
+        return list.remove(0);
+    }
+
+    @Override
+    public T peek() {
+        if (list.size() == 0) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public T element() {
+        if (list.size() == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return list.get(0);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new NotImplementedException();
+    }
+}
