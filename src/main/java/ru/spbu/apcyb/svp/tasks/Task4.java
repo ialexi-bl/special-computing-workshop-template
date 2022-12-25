a968a64d2f69f5dd59ebe89c70018bd6a5ee7855
Alex <ialexi.bl@gmail.com>
2022-12-25 17:46:56 +0300

    Fixed threads

--- src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
+++ src/main/java/ru/spbu/apcyb/svp/tasks/Task4.java
@@ -8,12 +8,9 @@ import org.apache.logging.log4j.core.config.Configurator;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 /**
@@ -56,14 +53,15 @@ public class Task4 {
     public static List<Double> readValues(String path) throws IOException {
         var file = Path.of(path);
         var result = new ArrayList<Double>();
-        var scanner = new Scanner(file);
 
-        while (scanner.hasNextDouble()) {
-            result.add(scanner.nextDouble());
-        }
+        try (var scanner = new Scanner(file)) {
+            while (scanner.hasNextDouble()) {
+                result.add(scanner.nextDouble());
+            }
 
-        if (scanner.hasNext()) {
-            throw new IllegalArgumentException("File may contain only double numbers separated by spaces");
+            if (scanner.hasNext()) {
+                throw new IllegalArgumentException("File may contain only double numbers separated by spaces");
+            }
         }
 
         return result;
@@ -82,28 +80,21 @@ public class Task4 {
 
     public static List<Double> calculateTangentsMultithread(final List<Double> values) {
         final int threadsCount = 10;
-        var executor = Executors.newFixedThreadPool(threadsCount);
-        var result = new Double[values.size()];
+        var result = new ArrayList<Double>(values);
 
-        int s = values.size();
-        int step = (int) Math.ceil((double) s / (double) threadsCount);
-        for (int i = 0; i < threadsCount; i++) {
-            int threadIndex = i;
-            executor.execute(() -> {
-                int from = step * threadIndex;
-                int to = Math.min(step * (threadIndex + 1), s);
-                for (int j = from; j < to; j++) {
-                    result[j] = Math.tan(values.get(j));
-                }
-            });
+        try (ExecutorService executor = Executors.newFixedThreadPool(threadsCount)) {
+            int s = values.size();
+            var list = Collections.synchronizedList(result);
+
+            for (int i = 0; i < s; i++) {
+                int threadIndex = i;
+                executor.execute(() -> {
+                    var tan = Math.tan(list.get(threadIndex));
+                    list.set(threadIndex, tan);
+                });
+            }
         }
 
-        try {
-            executor.shutdown();
-            executor.awaitTermination(10, TimeUnit.SECONDS);
-        } catch (InterruptedException ignored) {
-        }
-
-        return Arrays.asList(result);
+        return result;
     }
 }
